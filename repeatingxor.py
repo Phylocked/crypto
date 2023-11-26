02116346df48e285a11fb8c0585d6c1eb47a12d0
Phylocked <95604062+Phylocked@users.noreply.github.com>
2023-11-26 11:52:58 +0800

    更改了repeatingxor

--- repeatingxor.py
+++ repeatingxor.py
@@ -1,13 +1,20 @@
 from xor import byte_xor
+
+
+def repeat_xor(key: bytes, text: bytes) -> bytes:
+    r_key = b''
+    while len(r_key) < len(text):
+        r_key += key
+    r_key = r_key[0:len(text)]
+    return byte_xor(r_key, text)
+
+
 if __name__ == '__main__':
-    with open('5.txt','r') as file:
+    with open('5.txt', 'r') as file:
         plain_text = file.read().strip()
-    key = 'ICE'
-    repeated_key = ''
-    while len(repeated_key) < len(plain_text):
-        repeated_key += key
-    repeated_key = repeated_key[0:len(plain_text)]
-    bytes_repeated_key = repeated_key.encode()
-    bytes_plain_text = plain_text.encode()
-    bytes_cipher_text = byte_xor(bytes_repeated_key,bytes_plain_text)
-    print(bytes_cipher_text)
+    my_key = 'ICE'
+    b_key = my_key.encode()
+    b_plain_text = plain_text.encode()
+    result = repeat_xor(b_key, b_plain_text)
+    print(result)
+    
